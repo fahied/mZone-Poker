c876dde4e81f046255cacc3f1392c57ff52b43e0
ingo <ingocraft118@gmail.com>
2016-07-28 14:33:11 +0800

    code

---
+++ mZone Poker/OCViewController.h
@@ -0,0 +1,13 @@
+//
+//  OCViewController.h
+//  mZone Poker
+//
+//  Created by ingo on 7/27/16.
+//  Copyright © 2016 BHouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OCViewController : UIViewController
+
+@end
---
+++ mZone Poker/OCViewController.m
@@ -0,0 +1,40 @@
+//
+//  OCViewController.m
+//  mZone Poker
+//
+//  Created by ingo on 7/27/16.
+//  Copyright © 2016 BHouse. All rights reserved.
+//
+
+#import "OCViewController.h"
+
+@interface OCViewController ()
+
+@end
+
+@implementation OCViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+#ifdef YT_SERVER
+    self.view.backgroundColor = [UIColor redColor];
+#endif
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
